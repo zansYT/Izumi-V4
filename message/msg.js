aa2040741daf3e2b7dbca76b32e62ea346cb9c69
Arasya Rafi <94653668+GetSya@users.noreply.github.com>
2022-03-24 07:24:05 +0700

    Add files via upload

--- message/msg.js
+++ message/msg.js
@@ -1852,7 +1852,6 @@ case prefix+'readmore':
     break
 case prefix+'jo':
   case prefix+'simi':
-   if (isGroup)return reply("Hanya bisa di lakukan di chat pribadi:)")
   const cimcimi = await fetchJson(`https://api.simsimi.net/v2/?text=${q}&lc=id`)
   conn.sendMessage(from, { text: cimcimi.success})
   break
