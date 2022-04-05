96714f0096facb5c62b445e0083c8dcdd029c101
Arasya Rafi <94653668+GetSya@users.noreply.github.com>
2022-04-05 20:05:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -119,6 +119,7 @@ const connectToWhatsApp = async () => {
 	
 	conn.ev.on('group-participants.update', async (data) => {
 	try {
+	let metadata = await conn.groupMetadata(data.id)
 	  for (let i of data.participants) {
 		try {
 		  var pp_user = await conn.profilePictureUrl(i, 'image')
@@ -126,9 +127,9 @@ const connectToWhatsApp = async () => {
 		  var pp_user = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 		}
 		if (data.action == "add") {
-		  conn.sendMessage(data.id, { image: { url: pp_user }, caption: `Welcome @${i.split("@")[0]}`, mentions: [i] })
+		  conn.sendMessage(data.id, { image: { url: pp_user }, caption: `Welcome @${i.split("@")[0]} To The Group ${metadata.subject}`, mentions: [i] })
 		} else if (data.action == "remove") {
-		  conn.sendMessage(data.id, { image: { url: pp_user }, caption: `Sayonara @${i.split("@")[0]}`, mentions: [i] })
+		  conn.sendMessage(data.id, { image: { url: pp_user }, caption: `Goodbye @${i.split("@")[0]}`, mentions: [i] })
 		}
 	  }
 	} catch (e) {
